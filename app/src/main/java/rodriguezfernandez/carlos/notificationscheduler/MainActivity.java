f505ab6ffbd5d44c3990e2cc01684851fc328d25
Carlos <carloscoucho@gmail.com>
2019-04-08 00:43:37 +0200

    Task 2: Añadir la funcionalidad de lanzar la tarea. Añadir los switchs para configurar el Job, si está conectado a la energia y si está el dispositivo en reposo.

--- app/src/main/java/rodriguezfernandez/carlos/notificationscheduler/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/notificationscheduler/MainActivity.java
@@ -1,13 +1,119 @@
 package rodriguezfernandez.carlos.notificationscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //definir id y Scheduler.
+    private static final int JOB_ID = 0;
+    private JobScheduler mScheduler;
+
+    // Definir los switchs para equipo en reposo y equipo cargando.
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+
+    // Definir la barra
+    private SeekBar mSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Recuperar de R los elementos necesarios.
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
+        mSeekBar = findViewById(R.id.seekBar);
+
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        // Actualizar  texto vinculado a la barra.
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (i > 0) {
+                    seekBarProgress.setText(getString(R.string.seconds, i));
+                } else {
+                    seekBarProgress.setText(R.string.not_set);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+    }
+
+    // Programar la tarea segun los datos.
+    public void scheduleJob(View view) {
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        int seekBarInteger = mSeekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+        boolean constraintSet = selectedNetworkOption!= JobInfo.NETWORK_TYPE_NONE|| mDeviceChargingSwitch.isChecked()|| mDeviceIdleSwitch.isChecked() || seekBarSet;
+
+        if (constraintSet) {
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+            Toast.makeText(this, R.string.job_scheduled, Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            Toast.makeText(this, R.string.no_constraint_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Cancelar las tareas al pulsar en el boton.
+    public void cancelJobs(View view) {
+
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, R.string.jobs_canceled, Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
